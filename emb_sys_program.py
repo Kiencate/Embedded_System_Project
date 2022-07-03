7ed67aac9356fc69292c49fa71ee86e372e883b9
kiencate <kiencate@gmail.com>
2022-07-03 21:33:58 +0700

    update main program

--- emb_sys_program.py
+++ emb_sys_program.py
@@ -1,10 +1,11 @@
 import os
 from urllib import response
 import requests
-
+import time
 
 class ATM_Pins():
     rfid = None
+    check_card = None
     usr_info = None
     url = "http://10.133.161.237:4000/"
 
@@ -12,7 +13,11 @@ class ATM_Pins():
         dev = os.open("/dev/rfid_rc522_dev", os.O_RDONLY)
         while (self.rfid == None):
             self.rfid = os.read(dev, 4)
-        print(int(self.rfid[0]))
+        if self.rfid != b'':
+            self.check_card = True
+        else:
+            self.check_card = False
+        print(self.rfid)
         os.close(dev)
 
     def Display_Lcd(self, text):
@@ -53,9 +58,35 @@ class ATM_Pins():
         except:
             print("no user")
             return 0
+    def Identification(self):
+        self.Read_RFID()
+        if not self.check_card:
+            self.Display_Lcd("Invalid card num\n Pls try again!")
+            return False
+        else:
+            #check data base and return check card (true or false)
+            pass
+        if not self.check_card:
+            self.Display_Lcd("Invalid user\n Pls try again!")
+            return False
+        else:
+            return True
 
 
 test = ATM_Pins()
-test.Display_Lcd("Hello,\nPls insert tag")
-test.Read_RFID()
+test.Display_Lcd("Hello,\nPls insert card")
+start = time.time()
+while 1:
+    if (test.Identification()):
+        ## doi tra pin
+        print("doi thanh cong")
+        pass
+    else:
+        time_out = time.time()
+        if time_out - start > 5:
+            test.Display_Lcd("Hello,\nPls insert card")
+            start = time_out
+
+    
+
 

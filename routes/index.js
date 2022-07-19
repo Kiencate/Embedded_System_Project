aadbb8414fbb1f55d7a6264e576d63251e3d2f50
hqtruong <hatruong2k31@gmail.com>
2022-07-19 22:55:48 +0700

    update 15/07

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,13 @@ var battery = require("./battery");
 var home = require("./home");
 var login = require("./login");
 var user = require("./user");
+var payment = require("./payment");
 // var card = require("./card");
 
 function route(app) {
   app.use("/home", home);
   app.use("/user", user);
+  app.use("/payment", payment);
   app.use("/atm", atm);
   app.use("/battery", battery);
   app.use("/", login);
---
+++ routes/payment.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var app = express();
+
+app.get("/", function (req, res, next) {
+  if (req.session.loggedin) {
+    req.getConnection(function (error, conn) {
+      conn.query(
+        "SELECT * FROM payment ORDER BY Id DESC",
+        function (err, rows, fields) {
+          //if(err) throw err
+          if (err) {
+            req.flash("error", err);
+            res.render("payment/list", {
+              title: "",
+              data: "",
+            });
+          } else {
+            // render to views/user/list.ejs template file
+            res.render("payment/list", {
+              title: "",
+              data: rows,
+            });
+          }
+        }
+      );
+    });
+  } else {
+    req.flash("error", "Please login first!");
+    res.redirect("/login");
+  }
+});
+
+module.exports = app;

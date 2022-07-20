b4cbef9c3e50e9b62dbc0c6f5c043cca93e260c0
hqtruong <hatruong2k31@gmail.com>
2022-07-20 15:31:46 +0700

    update 16/07

--- routes/home.js
+++ routes/home.js
@@ -6,12 +6,4 @@ app.get("/", function (req, res, next) {
   res.render("home", { title: "" });
 });
 
-/**
- * We assign app object to module.exports
- *
- * module.exports exposes the app object as a module
- *
- * module.exports should be used to return the object
- * when this file is required in another module like app.js
- */
 module.exports = app;
